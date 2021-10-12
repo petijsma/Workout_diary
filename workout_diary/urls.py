1336ff7bae2535cdc7d006055df8aeab099d8036
Matej <matej.sip@gmail.com>
2021-10-12 09:16:09 +0200

    change of urls

--- workout_diary/urls.py
+++ workout_diary/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("workouts", views.WorkoutListView.as_view(), name="workout-list"),
-    path("workouts/<int:pk>", views.WorkoutDetailView.as_view(), name="workout-detail"),
-    path("workouts/contact", views.contact, name="contact"),
+    path("", views.WorkoutListView.as_view(), name="workout-list"),
+    path("<int:pk>", views.WorkoutDetailView.as_view(), name="workout-detail"),
+    path("contact", views.contact, name="contact"),
 ]

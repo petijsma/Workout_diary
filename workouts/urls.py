ea1161ea4b5df2b15cf4f95b6b5775b948fb8b1b
Matej <matej.sip@gmail.com>
2021-10-14 14:21:52 +0200

    1) enabling modification of the diary on front end using CreateView, UpdateView, DeleteView.
    2) locking of possible modification on frontend. Only verified user with
    password can add, alter or delete the workouts.
    
    3) adding a message informing of workout been added, altered or deleted.

--- workouts/urls.py
+++ workouts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("", views.WorkoutListView.as_view(), name="workout-list"),
     path("<int:pk>", views.WorkoutDetailView.as_view(), name="workout-detail"),
     path("contact", views.contact, name="contact"),
+    path("create", views.WorkoutCreateView.as_view(), name="workout-create"),
+    path("<int:pk>/update", views.WorkoutUpdateView.as_view(), name="workout-update"),
+    path("<int:pk>/delete", views.WorkoutDeleteView.as_view(), name="workout-delete"),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -1,13 +1,19 @@
-from django.views.generic import ( ListView, DetailView,)
+from django.views.generic import ( ListView, DetailView, CreateView, UpdateView, DeleteView,)
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.http import Http404
 from django.urls import reverse
 from django.core.mail import send_mail
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Workout
 
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
 class WorkoutListView(ListView):
     model = Workout
     queryset = Workout.objects.all().order_by("-date_of_workout")
@@ -31,3 +37,25 @@ def contact(request):
         return render(request, 'workouts/contact.html', {'message_name': message_name})
     else:
         return render(request, 'workouts/contact.html', {})
+
+class WorkoutCreateView(LockedView, SuccessMessageMixin, CreateView):
+    model = Workout
+    fields = ["title", "content", "date_of_workout"]
+    success_url = reverse_lazy("workout-list")
+    success_message = "You added new entry!"
+
+class WorkoutUpdateView(LockedView, SuccessMessageMixin, UpdateView):
+    model = Workout
+    fields = ["title", "content", "date_of_workout"]
+    success_message = "Your entry was updated!"
+
+    def get_success_url(self):
+        return reverse_lazy("workout-list")
+class WorkoutDeleteView(LockedView, DeleteView):
+    model = Workout
+    success_url = reverse_lazy("workout-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
